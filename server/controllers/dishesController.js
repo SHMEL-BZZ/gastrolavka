60105ffed712e57743d3e09882580db8ccb3b6ba
Ekaterina Andreeva <ekaterinaandreeva370@gmail.com>
2026-05-24 17:46:39 +0300

    Реализована базовая логика отправления запросов в usersController.js при регистрации пользователя. Добавлены jwt токены

--- server/controllers/dishesController.js
+++ server/controllers/dishesController.js
@@ -1,12 +1,48 @@
-class DishesController {
-    async create(req, res) {
+const path = require("path");
+const uuid = { v4: require('uuid').v4 };
+const ApiError = require("../error/ApiError");
+const {Dish, DishCategory, UnitOfMeasure} = require("../models/models");
+const {where} = require("sequelize");
 
+class DishesController {
+    async create(req, res, next) {
+        try{
+            const {name, description, price, category_id, weight, storage_time, units_of_measure, is_availible, calories, proteins, carbons, fats} = req.body
+            const {picture} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            picture.mv(path.resolve(__dirname, '..', 'static', fileName));
+            const dishes = await Dish.create({name, description, price, picture : fileName, category_id, weight, storage_time, units_of_measure, is_availible, calories, proteins, carbons, fats})
+            return res.json(dishes)
+        } catch(e){
+            next(ApiError.badRequest(e.message))
+        }
     }
     async getAll(req, res) {
-
+        let {category_id, is_availible, limit, page} = req.query
+        page = page || 1
+        limit = limit || 20
+        // отступ
+        let offset = page * limit - limit
+        //  сортировка по категории и доступности
+        let dishes;
+        if (!category_id && !is_availible) {
+            dishes = await Dish.findAndCountAll({ limit, offset });
+        } else if (!category_id && is_availible) {
+            dishes = await Dish.findAndCountAll({ where: { is_availible }, limit, offset });
+        } else if (category_id && is_availible) {
+            dishes = await Dish.findAndCountAll({ where: { category_id, is_availible }, limit, offset });
+        } else if (category_id && !is_availible) {
+            dishes = await Dish.findAndCountAll({ where: { category_id }, limit, offset });
+        }
+        return res.json(dishes)
     }
     async getOne(req, res) {
-
+        const {id} = req.params;          // параметр из роута (например, /api/dishes/1)
+        const dish = await Dish.findOne({
+            where: { dish_id: id },       // явно указываем dish_id
+            include: [DishCategory, UnitOfMeasure]
+        });
+        return res.json(dish);
     }
 
 }
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,8 +1,29 @@
+const {User} = require("../models/models");
 const ApiError = require("../error/ApiError");
+const sequelize = require('../db');
+const jwt = require('jsonwebtoken');
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const { username, email, password, phone, role_id } = req.body;
+        if (!username || !email || !password || !phone) {
+            return next(ApiError.badRequest('Все поля должны быть заполнены!'));
+        }
 
+        try {
+            const user = await sequelize.query(
+                'SELECT register_user($1, $2, $3, $4, $5) AS user_id',
+                {
+                    bind: [username, email, password, phone, role_id],
+                    type: sequelize.QueryTypes.SELECT
+                }
+            );
+
+            const token = jwt.sign({id: user.id, email, role_id }, process.env.SECRET_KEY, {expiresIn: '12h'});
+            return res.json({token});
+        } catch (e) {
+            next(ApiError.internal(e.message));
+        }
     }
     async login(req, res) {
 
