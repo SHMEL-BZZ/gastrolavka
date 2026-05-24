857c2c6e18178ecb4d197100f3ad02c9fa31de51
Ekaterina Andreeva <ekaterinaandreeva370@gmail.com>
2026-05-24 18:04:20 +0300

    Реализована базовая логика отправления запросов в usersController.js при авторизации пользователя.

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,6 +3,10 @@ const ApiError = require("../error/ApiError");
 const sequelize = require('../db');
 const jwt = require('jsonwebtoken');
 
+const generateToken = (id, email, role) => {
+        return jwt.sign({id, email, role }, process.env.SECRET_KEY, {expiresIn: '12h'});
+}
+
 class UserController {
     async registration(req, res, next) {
         const { username, email, password, phone, role_id } = req.body;
@@ -12,22 +16,45 @@ class UserController {
 
         try {
             const user = await sequelize.query(
-                'SELECT register_user($1, $2, $3, $4, $5) AS user_id',
+                'SELECT register_user($1, $2, $3, $4, $5) AS id',
                 {
                     bind: [username, email, password, phone, role_id],
                     type: sequelize.QueryTypes.SELECT
                 }
             );
 
-            const token = jwt.sign({id: user.id, email, role_id }, process.env.SECRET_KEY, {expiresIn: '12h'});
+            const token = generateToken(user.id, email, role_id);
             return res.json({token});
         } catch (e) {
             next(ApiError.internal(e.message));
         }
     }
-    async login(req, res) {
+    async login(req, res, next) {
+        const { email, password } = req.body;
 
+        try {
+            // Вызываем функцию, которая возвращает все поля (id, role_id и т.д.)
+            const result = await sequelize.query(
+                'SELECT * FROM authenticate_user($1, $2)',
+                {
+                    bind: [email, password],
+                    type: sequelize.QueryTypes.SELECT
+                }
+            );
+
+            // Проверяем, найден ли пользователь
+            if (!result || result.length === 0) {
+                return next(ApiError.badRequest('Неверный email или пароль'));
+            }
+
+            const user = result[0]; // берём первую запись
+            const token = generateToken(user.id, email, user.role_id);
+            return res.json({ token });
+        } catch (e) {
+            next(ApiError.internal(e.message));
+        }
     }
+
     async check(req, res, next) {
         const {id} = req.query;
         if (!id) {
